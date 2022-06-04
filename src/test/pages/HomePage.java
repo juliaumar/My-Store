9be486edd41207c6ab0014e4d0bf764e59cc929f
juliaumar <juliaumar71@gmail.com>
2022-06-04 10:26:25 -0400

    new Methods

---
+++ src/test/pages/HomePage.java
@@ -0,0 +1,14 @@
+package pages;
+
+import base.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePage extends BasePage {
+    protected WebDriver driver;
+
+    public HomePage(WebDriver driver){
+        super(driver);
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }}
